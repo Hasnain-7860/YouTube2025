daf80f3cf26decbbc92a75647d9b8baf57d40bdf
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-03-24 03:01:46 +0530

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Sidebar from './Components/Sidebar'
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Sidebar/>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+import logo from '../../public/logo.png'
+import { CiSearch } from "react-icons/ci";
+import { IoMdMic } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { AiOutlineBell } from "react-icons/ai";
+import profile from '../../public/profile.jpg';
+
+
+
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between px-6 pt-2'>
+        <div className=' flex justify-center items-center space-x-4'><RxHamburgerMenu className='text-xl cursor-pointer' /><img className='w-28 cursor-pointer' src={logo} alt="" /></div>
+        <div className=' flex w-[35%] items-center'>
+           <div className='w-[100%] px-3 py-2  border rounded-l-full'> 
+            <input className='outline-none' type="text" placeholder='Search'  />
+            </div>
+           <button className=' px-4 py-2 border rounded-r-full bg-gray-100'><CiSearch className='text-[25px]' /></button>
+           <IoMdMic className='text-[42px] ml-3 border rounded-full p-2 cursor-pointer hover:bg-gray-200 duration-200' />
+        </div>
+        <div className=' flex space-x-5 items-center'>
+        <RiVideoAddLine  className='text-2xl'/>
+        <AiOutlineBell className='text-2xl' /> 
+        <img className='w-[32px] rounded-[50%]' src={profile} alt="" />
+        </div>
+    </div>
   )
 }
 
